ee1a8e3d406d7a72c147d06132d05274620126af
hafizkalam <hafizkalamabdillah@google.com>
2021-12-22 00:08:54 +0700

    Test Case Announcement

---
+++ cypress/integration/3-testing-HMTI/pengumuman2.spec.js
@@ -0,0 +1,9 @@
+describe('Test Pengumuman 2', () => {
+    it('Test Case Announcement', () => {
+      cy.visit('https://alumni.polinema.ac.id/')
+      
+      cy.get(':nth-child(4) > a').click()
+      cy.get('input[type="submit"]').click()
+     
+    })
+})
---
+++ cypress/integration/3-testing-HMTI/pengumuman4.spec.js
@@ -0,0 +1,9 @@
+describe('Test Pengumuman 4', () => {
+    it('Test Case Announcement', () => {
+      cy.visit('https://alumni.polinema.ac.id/site/pengumuman')
+      
+      cy.get(':nth-child(1) > input').type('abc')
+      cy.get('[type="submit"]').click()
+     
+    })
+})
