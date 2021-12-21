44910baef8abe40fbcf8ddcf5c10852da39ebd36
hafizkalam <hafizkalamabdillah@google.com>
2021-12-22 00:10:13 +0700

    Test Case Announcement

--- cypress/integration/3-testing-HMTI/pengumuman1.spec.js
+++ cypress/integration/3-testing-HMTI/pengumuman1.spec.js
@@ -1,7 +1,7 @@
 describe('Test Pengumuman 1', () => {
     it('Test Case Announcement', () => {
       cy.visit('https://alumni.polinema.ac.id/')
-      
+
       cy.get(':nth-child(4) > a').click()
      
     })
