cf3e29fb60ae644905a7ee1b501ed1cd69f204cd
Nazario Safariesqi <nazariotyo@gmail.com>
2021-12-22 11:39:31 +0700

    SearchTestingDataAlumni

--- cypress/integration/4-TestingDataAlumni/Test2.js
+++ cypress/integration/4-TestingDataAlumni/Test2.js
@@ -1,6 +1,10 @@
 context ('Testing Data Alumni', () =>{
     it('Testing Data Alumni', () =>{
         cy.visit('https://alumni.polinema.ac.id/site/alumni')
+
+        cy.contains('Data Alumni')
+        cy.contains('Pencarian Data')
+        
         cy.get('[id="search"]').type('rio')
         cy.get('input[type="submit"]').click()
     })
--- cypress/integration/4-TestingDataAlumni/Test5.js
+++ cypress/integration/4-TestingDataAlumni/Test5.js
@@ -1,15 +1,21 @@
 context ('Testing Data Alumni', () =>{
     it('Testing Data Alumni', () =>{
         cy.visit('https://alumni.polinema.ac.id/site/alumni')
-        cy.get('#pr').select('61304 - D4 MANAJEMEN PEMASARAN').should('have.value','61304')
-        cy.get('input[type="submit"]').click()
-        //cy.get('div[class="pagination"]').select('2').click()//
-        //cy.get('div[class="pagination"]').select('Last ›').click()//
-        cy.visit('https://alumni.polinema.ac.id/site/alumni/index/pr/61304/start/10')
-        cy.visit('https://alumni.polinema.ac.id/site/alumni/index/pr/61304/start/1230')
-    })
 
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        return false
-      })
+        cy.get('#pr')
+        .should('contain','SEMUA PRODI')
+        .invoke('attr','id')
+        .should('equal','pr')
+
+        cy.get('#pr').select('61304 - D4 MANAJEMEN PEMASARAN')
+        .should('have.value','61304')
+        
+        cy.get('input[type="submit"]').click()
+        cy.get('#frameContent > :nth-child(3) > :nth-child(2)').click()
+        cy.get(':nth-child(3) > [href="https://alumni.polinema.ac.id/site/alumni/index/pr/61304/start/1230"]').click()
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+          })
+    })
 })
