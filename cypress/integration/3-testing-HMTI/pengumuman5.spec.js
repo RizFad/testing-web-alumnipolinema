bd19eb15246cea6e3f53a5fc627d9fd90cdd4edd
hafizkalam <hafizkalamabdillah@google.com>
2021-12-22 18:15:19 +0700

    Assertions

--- cypress/integration/3-testing-HMTI/pengumuman5.spec.js
+++ cypress/integration/3-testing-HMTI/pengumuman5.spec.js
@@ -1,9 +1,14 @@
-describe('Test Pengumuman 4', () => {
-    it('Test Case Announcement', () => {
+
+  it('Test Case Announcement', function() {
+
       cy.visit('https://alumni.polinema.ac.id/site/pengumuman')
-      
+
       cy.get(':nth-child(1) > input').type('Toga')
-      cy.get('[type="submit"]').click()
-     
-    })
-})
+      
+      cy.get('[type="submit"]')
+      
+      let name = 'Toga'
+      expect(name).to.be.equal('Toga')
+
+  })
+
